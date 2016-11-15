33ffd4d43cf1828972fdcb6ad6845f130b72ebcd
Coe bassett <jonathon.bassett@mullenmail.com>
2016-11-15 10:38:17 -0700

    unfinished- add students to list

--- Student-Information-System/src/SISRunner.java
+++ Student-Information-System/src/SISRunner.java
@@ -1,13 +1,30 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 public class SISRunner
 	{
 		public static Scanner file;
+		public static ArrayList<Student> students=new ArrayList<Student>();
 		public static void main(String[] args) throws FileNotFoundException
 			{
 				file=new Scanner(new File("studentInformation (1).txt"));
 
 			}
+	public static void fillList()
+	{
+		while (file.hasNext())
+			{
+				String info=file.nextLine();
+				
+				students.add(new Student(null, null, 0, null, null, null, null, null, null));
+			}
 	}
+	public static int findSpaces(String str)
+	{
+		for (int i=0; i<str.length(); i++)
+			{
+				
+			}
+		return 0;
+	}
+}
 //Initial test Comment
